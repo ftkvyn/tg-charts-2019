2d9f0aadda4915a36e7b077b2d032b46cbdafadd
Andrei Marchenko <andrei.marchenko@rockwool.com>
2019-03-11 11:38:28 +0100

    Better drawing lines, scale

--- public/js/chart.js
+++ public/js/chart.js
@@ -17,47 +17,71 @@ const axis_color = "#f2f4f5";
 const radius = 2, thickness = radius * 2;
 
 let state = {
-	mx: width,
-	my: height,
+	mx: width * 0.7,
+	my: height  * 2,
 	zx: 0,
 	zy: 0
 }
+
+function calcScale () {
+	state.scale_x = width / (state.mx - state.zx);
+	state.scale_y = height / (state.my - state.zy);
+}
+calcScale();
 
 function drawAxis(){
 	ctx.beginPath();	
 	ctx.lineWidth = thickness;
 	ctx.strokeStyle = axis_color;
 	ctx.beginPath();
-	ctx.moveTo(thickness, 0);
-	ctx.lineTo(thickness, height - thickness);
-	ctx.lineTo(width, height - thickness);
+	ctx.moveTo(thickness, - height);
+	ctx.lineTo(thickness, 0 - thickness);
+	ctx.lineTo(width, 0 - thickness);
 	ctx.stroke();
 
 	ctx.font = "14px Arial";
-	ctx.fillText(`${state.zx},${state.zy}`, 10, height - 10);
-	ctx.fillText(`${state.mx + state.zx}`, width - 30, height - 10);
-	ctx.fillText(`${state.my + state.zy}`, 10, 10);
+	ctx.fillText(`${state.zx},${state.zy}`, 10, - 10);
+	ctx.fillText(`${state.mx + state.zx}`, width - 30, - 10);
+	ctx.fillText(`${state.my + state.zy}`, 10, - height + 10);
 }
 
-function drawLine(orig_x0, orig_y0, orig_x1, orig_y1){
-	let x0 = orig_x0 - state.zx, x1 = orig_x1 - state.zx,
-		y0 = - orig_y0 + state.zy, y1 = - orig_y1 + state.zy;
+function translateX (orig_x) {
+	return Math.floor((orig_x - state.zx) * state.scale_x);
+}
+
+function translateY (orig_y) {
+	return  Math.floor((- orig_y + state.zy) * state.scale_y);
+}
+
+function startDraw(orig_x0, orig_y0) {
 	ctx.lineWidth = thickness;
 	ctx.strokeStyle = '#3DC23F';
+	ctx.lineJoin = 'round';
+	// ctx.miterLimit = 1;
 	ctx.beginPath();
-	ctx.moveTo(x0, y0);
-	ctx.lineTo(x1, y1);
-	ctx.stroke();
 
-	ctx.fillStyle = '#3DC23F';
-	ctx.beginPath();
-	ctx.arc(x0, y0, radius * 0.8, 0, 2 * Math.PI);
-	ctx.fill();
-	ctx.beginPath();
-	ctx.arc(x1, y1, radius * 0.8, 0, 2 * Math.PI);
-	ctx.fill();
+	let x0 = translateX(orig_x0), y0 = translateY(orig_y0);
+	ctx.moveTo(x0, y0);
 }
 
+function drawNextPoint(orig_x, orig_y) {
+	let x = translateX(orig_x), y = translateY(orig_y);
+	ctx.lineTo(x, y);
+}
+
+function endDraw() {
+	ctx.stroke();
+}
+
+function drawLine(orig_x0, orig_y0, orig_x1, orig_y1) {
+	let x0 = orig_x0 - state.zx, x1 = orig_x1 - state.zx,
+		y0 = - orig_y0 + state.zy, y1 = - orig_y1 + state.zy;
+	
+	ctx.lineTo(x1, y1);
+	ctx.stroke();
+}
+
+ctx.translate(0, height + 0);
 drawAxis();
 
 // ctx.beginPath();
@@ -70,13 +94,13 @@ drawAxis();
 // ctx.lineTo(500, 500);
 // ctx.stroke();          // Render the path
 
-ctx.translate(0.5, height + 0.5);
-drawLine(0, 0, 100, 100);
-drawLine(100, 100, 200, 480);
-drawLine(200, 480, 300, 000);
-drawLine(300, 000, 400, 400);
-drawLine(400, 400, 500, 30);
-
+startDraw(0, 0);
+drawNextPoint(100, 100);
+drawNextPoint(200, 480);
+drawNextPoint(300, 000);
+drawNextPoint(400, 400);
+drawNextPoint(500, 30);
+endDraw();
 // let prev = 0;
 // let vel = 100; // vel px per second;
 // let left = 0;
