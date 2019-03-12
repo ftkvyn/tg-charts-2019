0e280a3aed23cba231908b5d34b459d1a3941e8b
unknown <ftkvyn@gmail.com>
2019-03-12 21:35:58 +0100

    Main chart size

--- public/js/chart.js
+++ public/js/chart.js
@@ -304,7 +304,7 @@ class Chart {
 const appEl = document.getElementById('app');
 const main_chart = document.getElementById('main_chart');
 const chart_map = document.getElementById('chart_map');
-const height = 450,
+const height = 350,
 	map_height = 45;
 
 const mainChart = new Chart(main_chart, height);
